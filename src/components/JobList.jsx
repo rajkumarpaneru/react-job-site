252ab7bfcd2a5c041e5c9b6bfb0614fd01bd4d96
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-03-21 21:30:50 +0545

    add Job component

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import jobs from '../jobs.json';
+
+import Job from './Job';
+
 const JobList = () => {
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -10,38 +14,12 @@ const JobList = () => {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           
 
-          <div className="bg-white rounded-xl shadow-md relative">
-            <div className="p-4">
-              <div className="mb-6">
-                <div className="text-gray-600 my-2">Full-Time</div>
-                <h3 className="text-xl font-bold">Senior React Developer</h3>
-              </div>
-
-              <div className="mb-5">
-               We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript...
-              </div>
-
-              <h3 className="text-indigo-500 mb-2">$70 - $80K / Year</h3>
-
-              <div className="border border-gray-100 mb-5"></div>
-
-              <div className="flex flex-col lg:flex-row justify-between mb-4">
-                <div className="text-orange-700 mb-3">
-                  <i className="fa-solid fa-location-dot text-lg"></i>
-                  Boston, MA
-                </div>
-                <a
-                  href="job.html"
-                  className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-                >
-                 Read More
-                </a>
-              </div>
-            </div>
-          </div>
+          {
+            jobs.map((job) => (<Job />))
+          }  
           
 
-          <div className="bg-white rounded-xl shadow-md relative">
+          {/* <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
               <div className="mb-6">
                 <div className="text-gray-600 my-2">Remote</div>
@@ -69,9 +47,9 @@ const JobList = () => {
                 </a>
               </div>
             </div>
-          </div>
+          </div> */}
 
-          <div className="bg-white rounded-xl shadow-md relative">
+          {/* <div className="bg-white rounded-xl shadow-md relative"> */}
             <div className="p-4">
               <div className="mb-6">
                 <div className="text-gray-600 my-2">Remote</div>
