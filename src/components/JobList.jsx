08fb7f843055a0274034b8796c0f6bcbdb6b7d92
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-03-22 21:06:27 +0545

    show only 3 jobs in homepage

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -15,7 +15,8 @@ const JobList = () => {
           
 
           {
-            jobs.map((job) => (<Job
+            jobs.slice(0,3)
+            .map((job) => (<Job
               key = {job.id} 
               id = {job.id}
               type = {job.type}
