faa0f07b040348e9427e04890a5d84b722411ea0
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-03-24 20:17:50 +0545

    add react-icon package, show FaMapMarker icon component

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,5 +1,7 @@
 import {React, useState} from 'react'
 
+import { FaMapMarker } from 'react-icons/fa'
+
 const Job = (props) => {
 
   const [showFullDescription, setShowFullDescription] = useState(false);
@@ -28,7 +30,7 @@ const Job = (props) => {
 
               <div className="flex flex-col lg:flex-row justify-between mb-4">
                 <div className="text-orange-700 mb-3">
-                  <i className="fa-solid fa-location-dot text-lg"></i>
+                  <FaMapMarker  className='inline text-lg mb-1 mr-2'/>
                   {props.location}
                 </div>
                 <a
