167fd797f07b7be6399e5a98aaf3057480822b1e
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-03-24 21:14:09 +0545

    create homepage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
-import React from 'react'
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobList from './components/JobList';
-import ViewAllJobs from './components/ViewAllJobs';
+import React from 'react';
+import { 
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider 
+} from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<HomePage />} />)
+);
 
 const App = () => {
-  return (
-    <>
-    <Navbar />
-    <Hero 
-    title="Get React Developer Jobs" 
-    subTitle="Find all React job that fits your skills and needs"
-    />
-
-    <HomeCards />
-
-    <JobList />
-
-    <ViewAllJobs />
-    </>
-  );
+  return <RouterProvider router={router}/>
 }
 
 export default App;
