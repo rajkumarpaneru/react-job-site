e55435eea3ada4da758c1f112927aeaeef70cb04
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-04-02 20:51:48 +0545

    fetch jobs from server

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-import jobs from '../jobs.json';
+import { useState, useEffect } from 'react';
+
+// import jobs from '../jobs.json';
 
 import Job from './Job';
 
 const JobList = ({isHome = true}) => {
-  const myJobs = isHome ? jobs.slice(0,3) : jobs;
+  // const myJobs = isHome ? jobs.slice(0,3) : jobs;
+
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:5050/jobs')
+        const data = await res.json();
+        setJobs(data);
+      } catch (error){
+        console.log('Error while fetching jobs.', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchJobs();
+  }, []);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -17,7 +37,7 @@ const JobList = ({isHome = true}) => {
           
 
           {
-            myJobs
+            jobs
             .map((job) => (<Job
               key = {job.id} 
               id = {job.id}
