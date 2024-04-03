10d07231edc59a45c78d298935edbb1eade917c4
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-04-03 19:40:33 +0545

    fetch job by id using react-router data loader

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,31 +1,37 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import Spinner from "../components/Spinner";
+import {useLoaderData } from "react-router-dom";
 
 const JobPage = () => {
-    const {id} = useParams();
-    const [job, setJob] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const job = useLoaderData();
+    // const [job, setJob] = useState(null);
+    // const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchJob = async () => {
-            try {
-              const apiUrl = `/api/jobs/${id}`;
-              const res = await fetch(apiUrl)
-              const data = await res.json();
-              setJob(data);
-            } catch (error){
-              console.log('Error while fetching job.', error);
-            } finally {
-              setLoading(false);
-            }
-          }
-          fetchJob();
-        }, []);
+    // useEffect(() => {
+    //     const fetchJob = async () => {
+    //         try {
+    //           const apiUrl = `/api/jobs/${id}`;
+    //           const res = await fetch(apiUrl)
+    //           const data = await res.json();
+    //           setJob(data);
+    //         } catch (error){
+    //           console.log('Error while fetching job.', error);
+    //         } finally {
+    //           setLoading(false);
+    //         }
+    //       }
+    //       fetchJob();
+    //     }, []);
+
+
     
-  return loading ? <Spinner /> : (
+  return (
     <h1>{job.title}</h1>
   );
 }
 
-export default JobPage;
+const jobLoader = async ({params}) => {
+    const res = await fetch(`/api/jobs/${params.id}`);
+    const data = await res.json()
+    return data;
+}
+
+export {JobPage as default, jobLoader};
