f12f04efdfcac513f98c8d6488e3722dd353755d
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-04-03 19:13:49 +0545

    add single jobs route

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage from './pages/JobPage';
 
 
 
@@ -18,6 +19,7 @@ const router = createBrowserRouter(
   <Route path='/' element={<MainLayout />} >
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
+    <Route path='/jobs/:id' element={<JobPage />} />
     <Route path='*' element={<NotFoundPage />}/>
   </Route>
   
