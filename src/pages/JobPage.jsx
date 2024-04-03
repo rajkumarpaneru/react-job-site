2388339b998d54c1280676b7e63f8ade74753278
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-04-03 19:25:58 +0545

    fetch job by id

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
 const JobPage = () => {
-  return (
-    <div>JobPage</div>
-  )
+    const {id} = useParams();
+    const [job, setJob] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchJob = async () => {
+            try {
+              const apiUrl = `/api/jobs/${id}`;
+              const res = await fetch(apiUrl)
+              const data = await res.json();
+              setJob(data);
+            } catch (error){
+              console.log('Error while fetching job.', error);
+            } finally {
+              setLoading(false);
+            }
+          }
+          fetchJob();
+        }, []);
+    
+  return loading ? <Spinner /> : (
+    <h1>{job.title}</h1>
+  );
 }
 
 export default JobPage;
