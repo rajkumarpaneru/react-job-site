e70525eb9c20086cb9f481ff69059100ab74d128
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-04-03 21:44:37 +0545

    add-job page with route

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, {jobLoader} from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
 
 
 
@@ -19,6 +20,7 @@ const router = createBrowserRouter(
   <Route path='/' element={<MainLayout />} >
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
+    <Route path='/jobs/create' element={<AddJobPage />} />
     <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
     <Route path='*' element={<NotFoundPage />}/>
   </Route>
